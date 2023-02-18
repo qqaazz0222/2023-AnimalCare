241f29021640bf887352b9ab90b49764f66d520a
qqaazz0222 <qqaazz0222@naver.com>
2023-02-18 10:28:03 +0900

    first commit

---
+++ Backend/server.py
@@ -0,0 +1,37 @@
+import io
+import os
+from flask import Flask, jsonify, request
+from flask import make_response
+from flask_cors import CORS, cross_origin
+import json
+from pickletools import read_uint1
+import torch
+from torchvision import models
+import torchvision.transforms as transforms
+from PIL import Image
+
+app = Flask(__name__)
+CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+# load YOLO model
+model = torch.hub.load('./dataset/train/weights/best.pt', source='local')
+
+# storing incoming images into POST communication
+def save_image(file):
+    file.save('./temp/'+ file.filename)
+
+# storing server logs
+# def create_log()
+
+@app.route('/')
+# cors error processing
+@cross_origin()
+def web():
+    html = '''
+        <h1>Animal Care Yolo API Server</h1>
+    '''
+    return html
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=3001, debug=True)
