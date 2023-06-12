caf49677cad82b2ca1ef99cb0384db9ac53826d6
bbageon <129655962+bbageon@users.noreply.github.com>
2023-06-13 00:24:11 +0900

    웹소켓 통신
    웹소켓 통신

--- Backend/__websocket__.py
+++ Backend/__websocket__.py
@@ -1,26 +1,24 @@
-from flask import Flask, render_template
-from flask_socketio import SocketIO, emit
+import requests
+from flask import Flask,jsonify
+from io import BytesIO
+from PIL import Image
 
 app = Flask(__name__)
-socketio = SocketIO(app)
 
-@app.route('/websocket')
-def index():
-    return render_template('index.html')
+@app.route('/receive_image', methods=['GET'])
+def receive_image():
+    image_url = "http://192.168.0.55:8000/static/image/captured_image.jpg"  # 이미지가 전송되는 서버의 URL
+    image_path = 'static/Jetbot_image/captured_image.jpg'  # 이미지를 저장할 경로
 
-@socketio.on('connect')
-def handle_connect():
-    print('Client connected')
+    # 이미지 다운로드
+    response = requests.get(image_url)
+    img = Image.open(BytesIO(response.content))
+    if response.status_code == 200:
+        with open(image_path, 'wb') as f:
+            f.write(response.content)
+        return jsonify({"msg" : "Image saved successfully"})
+    else:
+        return jsonify({"msg" : "Failed to receive image"})
 
-@socketio.on('disconnect')
-def handle_disconnect():
-    print('Client disconnected')
-
-@socketio.on('response')
-def handle_response(response_data):
-    # Process the response data received from the websocket server
-    message = response_data.get('message')
-    print('Response:', message)
-
-def start():
-    socketio.run(app, host='0.0.0.0', port='3002')
+if __name__ == '__main__':
+    app.run("0.0.0.0",debug=True, port=8000)
